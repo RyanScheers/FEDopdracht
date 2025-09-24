41b9d8d8fb1711bc9f5326f1b120f358610b81ee
RyanScheers <ryansch2709@gmail.com>
2025-09-24 09:44:40 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("hi");
+
+// selecteer de knoppen
+const openButton = document.querySelector("header section button"); 
+const sluitButton = document.querySelector("header nav button");  
+const deNav = document.querySelector("header nav");                
+
+// klik op hamburger → schuif menu naar rechts
+openButton.addEventListener("click", () => {
+  deNav.classList.add('open');
+});
+
+// klik op kruisje → schuif menu terug naar links
+sluitButton.addEventListener("click", () => {
+  deNav.classList.remove('open');
+});
+
+
+
+
+
+
+
