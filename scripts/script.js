fd0c49688bb4c20c4d4aff075fef6335040c1ec0
RyanScheers <ryansch2709@gmail.com>
2025-09-29 19:39:37 +0200

    laatste versie

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,34 @@ console.log("hi");
 // selecteer de knoppen
 const openButton = document.querySelector("header section button"); 
 const sluitButton = document.querySelector("header nav button");  
-const deNav = document.querySelector("header nav");                
+const deNav = document.querySelector("header nav");  
+
+
+const genresKnop = document.querySelector("main section:nth-of-type(8) button");
+const genresDiv = document.querySelector("main section:nth-of-type(8) ul");
+const buttonTekst = document.querySelector("main section:nth-of-type(8) button h4");
+
+const klantenserviceKnop = document.querySelectorAll("main section:nth-of-type(10) article button");
+
+
+
+klantenserviceKnop.forEach((button) => {
+  button.addEventListener("click", () => {
+    const bijbehorendeLijst = button.closest('article').nextElementSibling;
+    const icoon = button.querySelector("svg");
+
+    if (bijbehorendeLijst && bijbehorendeLijst.tagName === 'UL') {
+      bijbehorendeLijst.classList.toggle("open");
+
+      if (bijbehorendeLijst.classList.contains("open")) {
+        if (icoon) icoon.style.transform = "rotate(180deg)";
+      } else {
+        if (icoon) icoon.style.transform = "rotate(0deg)";
+      }
+    }
+  });
+});
+
 
 // klik op hamburger → schuif menu naar rechts
 openButton.addEventListener("click", () => {
@@ -15,6 +42,36 @@ openButton.addEventListener("click", () => {
 sluitButton.addEventListener("click", () => {
   deNav.classList.remove('open');
 });
+
+
+
+
+
+let isVergroot = false;
+
+
+genresKnop.addEventListener("click", () => {
+    if (isVergroot) {
+        // verberg ul
+        genresDiv.style.display = 'none';
+        buttonTekst.textContent = "Meer genres";
+        genresKnop.querySelector('svg').style.transform = 'rotate(90deg)';
+    } else {
+        // laat ul zien
+        genresDiv.style.display = 'grid';
+        buttonTekst.textContent = "Minder genres";
+        genresKnop.querySelector('svg').style.transform = 'rotate(270deg)';
+    }
+    
+    isVergroot = !isVergroot;
+});
+
+
+
+
+
+
+
 
 
 
